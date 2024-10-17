84038ca6bb49c55d4e8027f8d865a9c909cbbff5
Piyush khare <piyushkharejhs@gmail.com>
2024-10-17 16:49:22 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,1 @@
-'use client';
-import RazorpayButton from '@/components/frontend/TestingPaymentPage';
 
-export default function Home() {
-  return (
-    <div className="text-center text-4xl font-sans mt-8 ">
-      Welcome to DESIZNIDEAZ
-      <p className="text-center text-4xl font-sans mt-8">
-        Piyush is a very good guy, he always helps me. I really want to thank him!
-      </p>
-      <RazorpayButton amount={1} />
-    </div>
-  );
-}
