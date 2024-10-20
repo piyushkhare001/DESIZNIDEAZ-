dd6fc89154caa2006609a8e71c01787dcd819b36
YahyaSaadME <yahyasaad.me@gmail.com>
2024-10-20 12:12:13 +0530

    Conflict solved

--- src/app/(routes)/Login/page.tsx
+++ src/app/(routes)/Login/page.tsx
@@ -1,11 +1,11 @@
 
 
-import { Input } from "@/components/ui/input";
+import { Input } from "../../../components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import Image from "next/image";
 import React from "react";
 import { FcGoogle } from 'react-icons/fc';
-import loginimg from '../../assets/loginimg.jpg';
+import loginimg from '../../../assets/loginimg.jpg';
 import { Button } from "src/components/ui/button";
 
 const Signin =  () => {
